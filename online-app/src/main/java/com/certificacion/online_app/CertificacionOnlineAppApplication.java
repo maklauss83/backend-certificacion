d8d8b2916123a431cb16c55f6f7f3e6106361aa0
JUAN-V-GTI <juan.velasco@grupotimexico.com>
2025-09-17 20:28:47 -0600

    Primer commit desde Visual Studio

---
+++ online-app/src/main/java/com/certificacion/online_app/CertificacionOnlineAppApplication.java
@@ -0,0 +1,13 @@
+package com.certificacion.online_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificacionOnlineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificacionOnlineAppApplication.class, args);
+	}
+
+}
---
+++ online-app/src/test/java/com/certificacion/online_app/CertificacionOnlineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certificacion.online_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificacionOnlineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
